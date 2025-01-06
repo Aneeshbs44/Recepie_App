e6aba461ecdf53f4c45e13c4700611110ba989fc
Aneeshbs44 <anish107655@gmail.com>
2025-01-06 18:03:15 +0530

    Created entry file

---
+++ src/navigation/start.js
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+const start = () => {
+  return (
+    <View>
+      <Text>start</Text>
+    </View>
+  )
+}
+
+export default start
