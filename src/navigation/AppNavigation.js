4e1a245af69e63d9d72a57f90ccbc032707ae1a4
Aneeshbs44 <anish107655@gmail.com>
2025-01-10 18:06:45 +0530

    Updated appnavigator

---
+++ src/navigation/AppNavigation.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Homescreen from '../screens/Homescreen'; // Adjust path based on your structure
+
+const Stack = createNativeStackNavigator();
+
+export default function AppNavigation() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
