f2d926d12f95286621d3ee7d0dd69393a2690444
Aneeshbs44 <anish107655@gmail.com>
2025-01-10 18:07:05 +0530

    added homescreen file

---
+++ src/screens/Homescreen.js
@@ -0,0 +1,14 @@
+import { Text, View } from 'react-native'
+import React, { Component } from 'react'
+
+export class Homescreen extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Homescreen</Text>
+      </View>
+    )
+  }
+}
+
+export default Homescreen
