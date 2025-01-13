a228dd54c8ca6e9b06223c22c0a4dee65c56faaf
Aneeshbs44 <anish107655@gmail.com>
2025-01-13 18:10:19 +0530

    fixed the error navigationcontainer not nested by adding navigation independent tree

--- src/navigation/AppNavigation.js
+++ src/navigation/AppNavigation.js
@@ -1,16 +1,19 @@
+// AppNavigation.js
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer, NavigationIndependentTree } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Homescreen from '../screens/Homescreen'; // Adjust path based on your structure
+import Homescreen from '../screens/Homescreen';
 
 const Stack = createNativeStackNavigator();
 
 export default function AppNavigation() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <NavigationIndependentTree>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={Homescreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </NavigationIndependentTree>
   );
 }
