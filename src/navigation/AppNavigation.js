8785959b9c5ba84e9dccd8271499e4c965178dc9
Aneeshbs44 <anish107655@gmail.com>
2025-01-15 18:18:36 +0530

    changes in appnavigation

--- src/navigation/AppNavigation.js
+++ src/navigation/AppNavigation.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { NavigationContainer, NavigationIndependentTree } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Homescreen from '../screens/Homescreen';
+import WelcomeScreen from '../screens/WelcomeScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -10,8 +11,14 @@ export default function AppNavigation() {
   return (
     <NavigationIndependentTree>
       <NavigationContainer>
-        <Stack.Navigator>
+        <Stack.Navigator
+          initialRouteName="Welcome"
+          screenOptions={{
+            headerShown: false,
+          }}
+          >
           <Stack.Screen name="Home" component={Homescreen} />
+          <Stack.Screen name="Welcome" component={WelcomeScreen} />
         </Stack.Navigator>
       </NavigationContainer>
     </NavigationIndependentTree>
